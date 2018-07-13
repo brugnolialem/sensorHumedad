eaa8a9edbe571f9a8f3ef0570ce9e5211c2859ad
Unknown <ibrugnoli@focusingenieria.com.uy>
2018-07-13 16:31:13 -0300

    Agrego info buscada (sim800, mqtt, sms, email, mqtt)

---
+++ Info/test_mqtt/test_mqtt/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 7/12/2018 2:17:14 PM
+ * Author : Nacho
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
